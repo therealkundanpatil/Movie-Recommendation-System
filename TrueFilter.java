476284c53617945f10e41e12c3bffa9b8e1c4c73
therealkundanpatil <kp28021996@gmail.com>
2020-08-16 03:48:49 -0400

    -Implemented TrueFiltered

---
+++ TrueFilter.java
@@ -0,0 +1,19 @@
+/*
+ * TrueFilter.java
+ * 
+ * Version : 1.0		Date : 08/16/2020
+ * 
+ * Revision : $Log$
+ * 
+ */
+package movierecommendation;
+
+public class TrueFilter implements Filter{
+
+	@Override
+	public boolean satisfies(String id) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+}
